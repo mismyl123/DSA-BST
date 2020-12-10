266445ee2cb4bf08861f234485ac8ab4067ea75d
mismyl123 <mlalonde309@gmail.com>
2020-12-10 15:57:10 -0500

    first commit

---
+++ bst-drills.js
@@ -0,0 +1,27 @@
+
+const BinarySearch = require('./dsa-bst');
+
+function main() {
+  const BST = new BinarySearch();
+
+  BST.insert('E');
+  BST.insert('A');
+  BST.insert('S');
+  BST.insert('Y');
+  BST.insert('Q');
+  BST.insert('U');
+  BST.insert('E');
+  BST.insert('S');
+  BST.insert('T');
+  BST.insert('I');
+  BST.insert('O');
+  BST.insert('I');
+  BST.insert('N');
+
+
+
+
+  console.log(BST);
+}
+
+main();
